377c06c9a3380340e27540b002b8979886e8f1d5
Kunalbhaipatel <pkunal450@gmail.com>
2025-05-29 11:36:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+
+import streamlit as st
+from multi_well_comparison import render_multi_well_page
+
+st.set_page_config(page_title="Prodigy IQ Dashboard", layout="wide", page_icon="🔍")
+render_multi_well_page()
